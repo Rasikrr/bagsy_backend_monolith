93485ab9e11ad521d4564c5f7c4666d95fe18382
Rasikrr <rs.t.95@mail.ru>
2025-09-20 01:05:47 +0500

    migrate added to ci

--- scripts/schema.mk
+++ scripts/schema.mk
@@ -1,16 +1,11 @@
-MIGRATIONS_DIR=./migrations
+MIGRATIONS_DIR := ./migrations
+GOOSE := go run github.com/pressly/goose/v3/cmd/goose@v3.25.0
 
 migrate-up:
-	go install github.com/pressly/goose/v3/cmd/goose@latest
-	goose -dir $(MIGRATIONS_DIR) postgres "$(POSTGRES_DSN)" up
+	$(GOOSE) -dir $(MIGRATIONS_DIR) postgres "$(POSTGRES_DSN)" up
 
 migrate-down:
-	go install github.com/pressly/goose/v3/cmd/goose@latest
-	goose -dir $(MIGRATIONS_DIR) postgres "$(POSTGRES_DSN)" down
-
+	$(GOOSE) -dir $(MIGRATIONS_DIR) postgres "$(POSTGRES_DSN)" down
 
 create-migration:
-	go install github.com/pressly/goose/v3/cmd/goose@latest
-	goose -dir $(MIGRATIONS_DIR) postgres "$(POSTGRES_DSN)" create $(name) sql
-
-
+	$(GOOSE) -dir $(MIGRATIONS_DIR) create $(name) sql
