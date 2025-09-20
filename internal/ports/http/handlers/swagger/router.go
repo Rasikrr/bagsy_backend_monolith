eb9a6810bd77e94e164be4548ea6b7fbaeb271bb
Rasikrr <rs.t.95@mail.ru>
2025-09-20 11:35:39 +0500

    ci updated, small refactor in swagger

--- internal/ports/http/handlers/swagger/router.go
+++ internal/ports/http/handlers/swagger/router.go
@@ -2,20 +2,27 @@
 package swagger
 
 import (
+	"fmt"
+
 	_ "github.com/Rasikrr/bugsy_backend_monolith/docs/swagger"
 	"github.com/go-chi/chi/v5"
 	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 type Controller struct {
+	scheme string
+	host   string
 }
 
-func New() *Controller {
-	return &Controller{}
+func New(scheme, host string) *Controller {
+	return &Controller{
+		scheme: scheme,
+		host:   host,
+	}
 }
 
 func (c *Controller) Init(router *chi.Mux) {
 	router.Get("/swagger/*", httpSwagger.Handler(
-		httpSwagger.URL("http://localhost:8080/swagger/doc.json"),
+		httpSwagger.URL(fmt.Sprintf("%s://%s/swagger/doc.json", c.scheme, c.host)),
 	))
 }
