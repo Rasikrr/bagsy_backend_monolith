ae26ad4f6e1644df60ac97fdd4d1b1b4e2aad0e5
Rasikrr <rs.t.95@mail.ru>
2025-10-12 00:44:34 +0500

    Revert "entities"
    This reverts commit 64cfdcffabfc5d629c6ef141b8551f991bd408cb.

--- internal/domain/entity/point.go
+++ internal/domain/entity/point.go
@@ -3,20 +3,19 @@ package entity
 import "time"
 
 type Point struct {
-	Code         string        `json:"code"`
-	NetworkCode  string        `json:"network_code"`
-	Category     PointCategory `json:"category"`
-	Name         string        `json:"name"`
-	Description  string        `json:"description,omitempty"`
-	Address      Address       `json:"address,omitempty"`
-	City         string        `json:"city,omitempty"`
-	Schedule     []Schedule    `json:"schedule,omitempty"`
-	OpeningHours string        `json:"opening_hours,omitempty"`
-	CreatedAt    time.Time     `json:"created_at"`
-	UpdatedAt    *time.Time    `json:"updated_at"`
-	UpdatedBy    *string       `json:"updated_by,omitempty"`
-	Active       bool          `json:"active"`
-	DeletedAt    *time.Time    `json:"deleted_at,omitempty"`
+	Code         string     `json:"code"`
+	NetworkCode  string     `json:"network_code"`
+	Name         string     `json:"name"`
+	Description  string     `json:"description,omitempty"`
+	Latitude     float64    `json:"latitude"`
+	Longitude    float64    `json:"longitude"`
+	Address      string     `json:"address,omitempty"`
+	City         string     `json:"city,omitempty"`
+	OpeningHours string     `json:"opening_hours,omitempty"`
+	Schedule     []Schedule `json:"schedule,omitempty"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    time.Time  `json:"updated_at"`
+	UpdatedBy    *string    `json:"updated_by,omitempty"`
 }
 
 type Schedule struct {
@@ -25,15 +24,4 @@ type Schedule struct {
 	Close   string `json:"close,omitempty"`
 	AllDay  bool   `json:"all_day,omitempty"`
 	Comment string `json:"comment,omitempty"`
-}
-
-type Address struct {
-	Street      string      `json:"street,omitempty"`
-	City        string      `json:"city,omitempty"`
-	Coordinates Coordinates `json:"coordinates,omitempty"`
-}
-
-type Coordinates struct {
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
 }
--- internal/domain/entity/service.go
+++ internal/domain/entity/service.go
@@ -1,16 +1,7 @@
 package entity
 
-import "time"
-
 type Service struct {
-	ID              int64           `json:"id"`
-	Point           Point           `json:"point"`
-	Name            string          `json:"name"`
-	Description     string          `json:"description"`
-	Category        ServiceCategory `json:"category"`
-	Subcategory     ServiceCategory `json:"subcategory"`
-	DurationMinutes int             `json:"duration_minutes"`
-	Active          bool            `json:"active"`
-	CreatedAt       time.Time       `json:"created_at"`
-	UpdatedAt       *time.Time      `json:"updated_at"`
+	Category    string
+	Description string
+	Name        string
 }
