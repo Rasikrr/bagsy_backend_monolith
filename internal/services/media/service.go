11417e01a301bef711adffff411b380d331f1bf2
Rasikrr <rs.t.95@mail.ru>
2026-01-11 04:44:17 +0500

    media service created

--- internal/services/media/service.go
+++ internal/services/media/service.go
@@ -1,1 +1,22 @@
 package media
+
+import (
+	"context"
+	"time"
+)
+
+type storage interface {
+	GeneratePresignedUploadURL(ctx context.Context, key string, contentType string, expiresIn time.Duration) (string, error)
+	GeneratePresignedDownloadURL(ctx context.Context, key string, expiresIn time.Duration) (string, error)
+}
+
+type Service struct {
+	storage storage
+}
+
+// nolint
+func NewService(storage storage) *Service {
+	return &Service{
+		storage: storage,
+	}
+}
