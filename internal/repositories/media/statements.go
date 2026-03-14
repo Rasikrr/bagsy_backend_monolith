61ca36a9a764aa8c441bbbe1899170ebff45957f
Rasikrr <rs.t.95@mail.ru>
2026-03-14 17:06:21 +0500

    fix media upload

--- internal/repositories/media/statements.go
+++ internal/repositories/media/statements.go
@@ -20,7 +20,7 @@ const (
 	                        id, bucket, object_key, filename, mime_type,
 	                        size_bytes, status, created_at, updated_at
 	                ) VALUES (
-	                        	, $2, $3, $4, $5, $6, $7, $8, $9
+	                        $1, $2, $3, $4, $5, $6, $7, $8, $9
 	                ) ON CONFLICT (id) DO UPDATE SET
 	                        status = EXCLUDED.status,
 	                        updated_at = EXCLUDED.updated_at;
